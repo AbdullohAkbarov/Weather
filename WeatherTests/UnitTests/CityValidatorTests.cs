991a227dc178eef03605da69351aee010f286376
JakhongirAkbarov <58388580+JakhongirAkbarov@users.noreply.github.com>
2021-09-10 19:04:58 +0500

    Added new CityValidatorTest class

---
+++ WeatherTests/UnitTests/CityValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherBL.Validators;
+
+namespace WeatherTests.UnitTests
+{
+    [TestFixture]
+    public class CityValidatorTests
+    {
+        [Test]
+        public void Validate_CityIsNotNull_True()
+        {
+            //Arrange
+            var city = "London";
+            var cityValidator = new CityValidator();
+
+            //Act
+            var result = cityValidator.Validate(city);
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void Validate_CityIsNull_False()
+        {
+            //Arrange
+            string city = null;
+            var cityValidator = new CityValidator();
+
+            //Act
+            var result = cityValidator.Validate(city);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
--- WeatherTests/UnitTests/WeatherServiceTests.cs
+++ WeatherTests/UnitTests/WeatherServiceTests.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using System.Threading.Tasks;
 using WeatherBL;
 using WeatherBL.Models;
+using WeatherBL.Validators;
 using WeatherDAL;
 using WeatherDAL.Models;
 
@@ -19,11 +20,10 @@ namespace WeatherTests.UnitTests
             var city = "Tashkent";
             var mock = new Mock<IWeatherProvider>();
             mock.Setup(m => m.GetWeatherAsync(city)).ReturnsAsync(new WeatherResponse());
-            service = new WeatherService(mock.Object);
+            service = new WeatherService(mock.Object, new CityValidator());
 
             //Act
             var result = await service.GetCurrentWeatherAsync(city);
-
 
             //Assert
             Assert.AreEqual(result.City, city);
