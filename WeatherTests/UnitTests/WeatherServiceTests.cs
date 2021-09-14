cba2b61c9562e0665ba51a827994af1b6acb6ff3
JakhongirAkbarov <58388580+JakhongirAkbarov@users.noreply.github.com>
2021-09-14 17:11:43 +0500

    Added extra test

--- WeatherTests/UnitTests/WeatherServiceTests.cs
+++ WeatherTests/UnitTests/WeatherServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using NUnit.Framework;
+using System;
 using System.Threading.Tasks;
 using WeatherBL;
 using WeatherBL.Interfaces;
@@ -15,24 +16,25 @@ namespace WeatherTests.UnitTests
     {       
         private WeatherService _service;
         private WeatherService _testService;
+        private WeatherService _testProvider;
         private Mock<IWeatherProvider> _mockProvider;
+        private Mock<IWeatherProvider> _mockProvide;
         private Mock<IValidator> _mockValidator;
         private Mock<IValidator> _mockValidate;
         private double temp;
         private string city;
-        
+        private string emptyCity = "";
+
 
         public WeatherServiceTests()
         {
             temp = 300.00;
             city = "Tashkent";
             _mockProvider = new Mock<IWeatherProvider>();
-            _mockValidator = new Mock<IValidator>();
-            _mockValidate = new Mock<IValidator>();
-            _mockProvider.Setup(m => m.GetWeatherAsync(city)).ReturnsAsync(new WeatherResponse() { StatusCode = 200, Main = new Main() { Temp = temp } });
+            _mockValidator = new Mock<IValidator>();          
+            _mockProvider.Setup(m => m.GetWeatherAsync(city)).ReturnsAsync(new WeatherResponse() { StatusCode = 200, Main = new Main() { Temp = temp } });            
             _mockValidator.Setup(m => m.Validate(city)).Returns(true);
             _service = new WeatherService(_mockProvider.Object, _mockValidator.Object);
-            _testService = new WeatherService(_mockProvider.Object, _mockValidate.Object);
         }
         
         [Test]
@@ -60,21 +62,38 @@ namespace WeatherTests.UnitTests
         }
 
         [Test]
-        public async Task GetCurrentWeatherAsync_ResultsTypeIsWeatherModel_ReturnsTrue()
+        public async Task GetCurrentWeatherAsync_ValidatorIsFalse_ReturnsIsSuccessParameterIsFalse()
         {
             //Arrange
 
             //Act
-            var result = await _testService.GetCurrentWeatherAsync(city);
+            var result = await _service.GetCurrentWeatherAsync(emptyCity);
 
             //Assert
-            Assert.That(result, Is.InstanceOf(typeof(WeatherModel)));
+            Assert.That(result.IsSuccess, Is.False);
+        }
+
+        [Test]
+        public async Task GetCurrentWeatherAsync_ResultErrorAreEqualNullReferenceError_ReturnsAreEqual()
+        {
+            //Arrange
+            _mockProvide = new Mock<IWeatherProvider>();
+            _mockProvide.Setup(m => m.GetWeatherAsync(emptyCity)).ThrowsAsync(new NullReferenceException());
+            _testProvider = new WeatherService(_mockProvide.Object, _mockValidate.Object);
+
+            //Act
+            var result = await _testProvider.GetCurrentWeatherAsync(emptyCity);
+
+            //Assert
+            Assert.AreEqual(result.Error, "Unspecified Error Occurred. Object reference not set to an instance of an object.");
         }
 
         [Test]
         public async Task GetCurrentWeatherAsync_ValidatorIsEmpty_ReturnsParameterAreNullZeroFalse()
         {
             //Arrange
+            _mockValidate = new Mock<IValidator>();
+            _testService = new WeatherService(_mockProvider.Object, _mockValidate.Object);
 
             //Act
             var result = await _testService.GetCurrentWeatherAsync(city);
@@ -82,19 +101,6 @@ namespace WeatherTests.UnitTests
             //Assert
             Assert.That(result.City, Is.Null);
             Assert.That(result.Temperature, Is.Zero);
-            Assert.That(result.IsSuccess, Is.False);
-        }
-
-        [Test]
-        public async Task GetCurrentWeatherAsync_ValidatorIsFalse_ReturnsIsSuccessParameterIsFalse()
-        {
-            //Arrange
-            string emptyCity = "";
-
-            //Act
-            var result = await _service.GetCurrentWeatherAsync(emptyCity);
-
-            //Assert
             Assert.That(result.IsSuccess, Is.False);
         }
     }
